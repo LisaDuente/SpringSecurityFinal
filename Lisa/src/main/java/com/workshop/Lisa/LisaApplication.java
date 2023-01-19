d046969e54e2e3d64a38182f4a3b7d5a3f34289d
NiklasEdu <niklas.upperud@edu.newton.se>
2023-01-19 15:12:26 +0100

    småfix i main klassen

--- Lisa/src/main/java/com/workshop/Lisa/LisaApplication.java
+++ Lisa/src/main/java/com/workshop/Lisa/LisaApplication.java
@@ -1,11 +1,7 @@
 package com.workshop.Lisa;
 
-import com.workshop.Lisa.Dao.UserDao;
-import com.workshop.Lisa.Entity.User;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LisaApplication {
@@ -14,20 +10,4 @@ public class LisaApplication {
 		SpringApplication.run(LisaApplication.class, args);
 	}
 
-//	@Bean
-//	PasswordEncoder passwordEncoder(){
-//		return new BCryptPasswordEncoder();
-//	}
-
-//	@Bean
-//	CommandLineRunner run(UserDao userDao){
-//
-//		return args -> {
-//
-//			userDao.save(new User(null,"test", "test", "test", "test", "test", "USER"));
-//			userDao.save(new User(null, "test", "test", "test", "test", "test", "USER"));
-//			userDao.save(new User(null,"test", "test", "test", "test", "test", "USER"));
-//
-//		};
-//	}
 }
