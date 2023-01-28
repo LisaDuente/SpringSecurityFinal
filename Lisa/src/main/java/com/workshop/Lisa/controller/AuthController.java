f8e4c53fce380becadbde730f690bc42d654edbb
lucasakerlund <lucas2001@hotmail.se>
2023-01-28 16:12:26 +0100

    Fixade cors

--- Lisa/src/main/java/com/workshop/Lisa/controller/AuthController.java
+++ Lisa/src/main/java/com/workshop/Lisa/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.workshop.Lisa.Dto.UserRegisterDto;
 import com.workshop.Lisa.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
