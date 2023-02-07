561a1a0e58a1821dc4bb85d9eb3928e817436a0b
Lisa Dünte <lisa.duente@gmail.com>
2023-02-07 15:43:55 +0100

    Niklas+Toros+Lisa: fixed /uploadPicture and /getPicture

--- Lisa/src/main/java/com/workshop/Lisa/service/UserService.java
+++ Lisa/src/main/java/com/workshop/Lisa/service/UserService.java
@@ -177,7 +177,28 @@ public class UserService {
         return matcher.matchWithAllUsers(meUser.getPreferences(),meUser,allUsers);
     }
 
-   //fix a method for updatePreferences and update UserInformation to abstract those two things
+   public String uploadUserPicture(String username, String path){
+        System.out.println("picture path: "+path);
+       User existingUser = this.dao.findByUsername(username)
+               .orElseThrow(() -> new EntityNotFoundException("could not find user"));
+       existingUser.setPhoto(path);
+       this.dao.save(existingUser);
+       return "picture saved";
+   }
 
+   public byte[] getImage(String username){
+       User existingUser = this.dao.findByUsername(username)
+               .orElseThrow(() -> new EntityNotFoundException("could not find user"));
 
+       BufferedImage img = null;
+       try {
+           img = ImageIO.read(new File(existingUser.getPhoto()));
+           ByteArrayOutputStream baos = new ByteArrayOutputStream();
+           ImageIO.write(img, "jpg", baos);
+           return baos.toByteArray();
+       } catch (IOException e) {
+           System.out.println(e);
+       }
+       return null;
+   }
 }
